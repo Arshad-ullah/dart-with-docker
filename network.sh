2de0cd114c75b4c54fc4cf334d65b523abe88f16
Arshad ullah <jahan665577@gmail.com>
2026-08-10 21:57:04 +0500

    network module added

--- network.sh
+++ network.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-ifconfig
+current_directly=$(pwd)
+
+
+echo "current dirrectly=====>>: $current_directly"
+
+mkdir bin/messages
+
+
+cd bin/messages
+
+
+
+current_directly=$(pwd)
+
+echo "updated directly=====>>: $current_directly"
+
+
