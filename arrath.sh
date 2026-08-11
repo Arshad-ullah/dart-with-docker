78a7e667bc47656a8a0d65fcc71c0570d79b8ede
Arshad ullah <jahan665577@gmail.com>
2026-08-11 16:45:36 +0500

    issue solve

--- arrath.sh
+++ arrath.sh
@@ -6,12 +6,41 @@
 
 # =$((value+value2))
 
+number1=90
+
+number2=10
 
 
-number1=23
-
-number2=90
 
 
 
+# add two number
 echo "$(( number1+number2 ))"
+
+#subtract two number
+echo "$(( number1-number2 ))"
+
+# multiply two number
+echo "$(( number1*number2 ))"
+
+#Devision two numebr
+echo "$(( number1/number2 ))"
+
+
+
+echo "===============End of arrithmatic==============="
+
+
+
+
+read -p "Enter your name : " name
+
+
+echo "Your name is ${name}"
+
+
+
+
+
+
+
