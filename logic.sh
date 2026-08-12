14a62642bbf275e7c4889f51b283f3127a4fe7a6
Arshad ullah <jahan665577@gmail.com>
2026-08-12 09:11:39 +0500

    issue solve

--- logic.sh
+++ logic.sh
@@ -12,6 +12,21 @@ read -p "Enter second value: " numb2
 echo "first value:$numb    second value..$numb2"
 
 
+if [ "$numb" -eq "$numb2" ]; then
+
+    echo "First value is greater"
+
+
+else 
+    echo "Second value is greater"
+
+
+fi
+
+
+echo "========gt==============="
+
+
 if [ "$numb" -gt "$numb2" ]; then
 
     echo "First value is greater"
@@ -26,3 +41,60 @@ fi
 
 
 
+
+echo "========ge==============="
+
+if [ "$numb" -ge "$numb2" ]; then
+
+    echo "First value is greater"
+
+
+else 
+    echo "Second value is greater"
+
+
+fi
+
+
+echo "========lt==============="
+
+if [ "$numb" -lt "$numb2" ]; then
+
+    echo "First value is greater"
+
+
+else 
+    echo "Second value is greater"
+
+
+fi
+
+
+echo "========le==============="
+
+if [ "$numb" -le "$numb2" ]; then
+
+    echo "First value is greater"
+
+
+else 
+    echo "Second value is greater"
+
+
+fi
+
+
+
+
+echo "========ne==============="
+
+if [ "$numb" -ne "$numb2" ]; then
+
+    echo "First value is greater"
+
+
+else 
+    echo "Second value is greater"
+
+
+fi
