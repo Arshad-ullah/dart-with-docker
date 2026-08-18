8fea1b3b3784d8e5307d75df4bf98ef880cfa2f2
Arshad ullah <jahan665577@gmail.com>
2026-08-18 15:19:31 +0500

    issue solve

--- method.sh
+++ method.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
-myName(){
-    echo "$1 this is just first method"
+
+# simple function
+
+
+#
+# myFunction(){
+#     echo "$1 this is just first method"
+# }
+
+
+# myFunction "Hello"
+
+# pass multiple paramters
+
+
+
+# address(){
+    
+#     echo "My name is $1 and I belong to $2"
+# }
+
+
+# address Arshad "Karbogha sharif"
+
+
+
+# return value
+
+
+address(){
+
+    return  2
 }
 
 
-myName "Hello"
+address Arshad "Karbogha sharif"
+
+
+result=$?
+
+echo "$result"
